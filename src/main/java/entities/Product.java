dfe38a39203bebaf0a0197ffae17fc7e5b9c4e50
dohaammor4 <dohaammor4@gmail.com>
2025-10-18 23:13:08 +0100

    first commit

---
+++ src/main/java/entities/Product.java
@@ -0,0 +1,22 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double price;
+
+    public Product() {}
+
+    public void clone(String s) {
+    }
+
+    // Getters et Setters
+}
